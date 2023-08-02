b6466ba3d7889d1a795f961b81095511cde6c4fe
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 19:23:56 +0330

    render into Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+// add bootstrap css
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="whole">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
