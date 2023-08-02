2de5dd80c5bcedd55bfe6f83ce00c3da118cb6c2
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 20:20:22 +0330

    imported Meta with meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import Head from "./Head";
 import Nav from "./Nav";
+import Meta from "./Meta";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Meta />
       <Nav />
       <Head />
       {children}
