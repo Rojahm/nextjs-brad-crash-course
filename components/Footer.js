bd5efde0d9cc3d0fe32fd48cc34643bcfb1bc05a
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 19:23:11 +0330

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>OpenSource</span> coded by <span>Ro?a</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
